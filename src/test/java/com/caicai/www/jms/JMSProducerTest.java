bac8f9e1cd3b08752391f3d8ac2a1fea086432bb
caicai <837522223@qq.com>
2016-04-20 14:53:14 +0800

    修改JUNIT为4.11版本

---
+++ src/test/java/com/caicai/www/jms/JMSProducerTest.java
@@ -0,0 +1,20 @@
+package com.caicai.www.jms;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class JMSProducerTest {
+
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void testSendMessage(){
+		assertEquals(1, 1);
+	}
+	
+}
